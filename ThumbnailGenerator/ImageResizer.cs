e49233c9c8de2f83e5cad429812d3e9183a1ed77
Rokibul Islam <rokib.hridoy@gmail.com>
2023-05-11 18:41:46 +0600

    Add project files.

---
+++ ThumbnailGenerator/ImageResizer.cs
@@ -0,0 +1,50 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+
+namespace ThumbnailGenerator
+{
+    public static class ImageResizer
+    {
+        [FunctionName("ResizeImage")]
+        public static void Run(
+            [BlobTrigger("mlsdoctors/{name}", Connection = "AzureWebJobsStorage")] Stream inputBlob,
+            [Blob("mlsdoctors-200x200/{name}", FileAccess.Write)] Stream outputBlob,
+            string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function processed blob\n Name:{name} \n  Size: {inputBlob.Length} Bytes");
+
+            // Check the file extension
+            string extension = Path.GetExtension(name).ToLowerInvariant();
+            if (extension != ".jpg" && extension != ".jpeg" && extension != ".png")
+            {
+                log.LogWarning($"Unsupported file extension: {extension}");
+                return;
+            }
+
+            // Load the image and resize it
+            using Image image = Image.Load(inputBlob);
+            int height = 200;
+            int width = (int)Math.Round(height * (double)image.Width / image.Height);
+            image.Mutate(x => x.Resize(width, height, KnownResamplers.Lanczos3));
+
+            // Save the resized image to the output stream
+            switch (extension)
+            {
+                case ".jpg":
+                case ".jpeg":
+                    image.Save(outputBlob, new JpegEncoder());
+                    break;
+                case ".png":
+                    image.Save(outputBlob, new PngEncoder());
+                    break;
+            }
+        }
+    }
+}
+
